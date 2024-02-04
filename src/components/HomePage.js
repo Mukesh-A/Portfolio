2f81133455ae5fdf131d79a9be7bc6d624aa2766
Mukesh-a <mukeshabhi08@gmail.com>
2024-02-05 00:01:52 +0530

    updated

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../img3.png";
 import styled from "styled-components";
-
+import { HiOutlineDownload } from "react-icons/hi";
+import client from "../client";
 const HomePage = () => {
+  const [postData, setPost] = useState(null);
+
+  useEffect(() => {
+    client
+      .fetch(
+        `*[_type == "category"]|order(_createdAt asc){
+          skill
+    }`
+      )
+      .then((data) => setPost(data))
+      .catch(console.error());
+  }, []);
   return (
     <Container className="home" id="hero">
       {/* <span>`</span> */}
       <div className="home-content">
         <span className="main_text">Hi,I'm</span>
-        <span className="main_text_name"> Mukesh()</span>
-        {/* <span>`</span> */}
+        <span className="main_text_name"> {`<Mukesh />`}</span>
         <h3 className="fullstack">FullStack Developer</h3>
+        <span
+          className="npm"
+          onClick={() =>
+            postData[1].skill[0] && window.open(postData[1].skill[0])
+          }
+        >
+          {`> npm install @mukesh/resume `}
+          <HiOutlineDownload size={18} className="download--icon" />
+        </span>
       </div>
       <div className="img-container">
-        <img className="img-to-fit" src={logo} alt="Mukesh"/>
+        <img className="img-to-fit" src={logo} alt="Mukesh" />
       </div>
     </Container>
   );
@@ -38,7 +59,6 @@ const Container = styled.div`
 
   .home-content {
     width: 75%;
-
     animation: 2s home-content;
     font-size: 4rem;
     font-weight: 700;
@@ -59,6 +79,27 @@ const Container = styled.div`
       color: #682ae9;
 
       cursor: pointer;
+    }
+    .npm {
+      font-family: ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas,
+        Liberation Mono, Courier New, monospace;
+      font-size: 1rem;
+      color: #dbdbdb;
+      font-weight: 500;
+      outline: 1px inset #777;
+      border-radius: 5px;
+      padding: 0.7rem;
+      display: inline-block;
+      cursor: pointer;
+      @media (max-width: 480px) {
+        font-size: 0.8rem;
+      }
+    }
+    .download--icon {
+      color: #777;
+
+      vertical-align: bottom;
+      font-size: 1rem;
     }
     .fullstack {
       color: #777;
@@ -81,8 +122,7 @@ const Container = styled.div`
       }
     }
   }
-  
-   
+
   .img-container {
     /* border: 2px solid red; */
     justify-content: center;
