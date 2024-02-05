4b8ad150d1727d5554b49b651dcabe686c92433c
Mukesh-a <mukeshabhi08@gmail.com>
2024-02-05 15:37:16 +0530

    Final

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -108,6 +108,7 @@ const Container = styled.div`
       box-shadow: rgba(161, 9, 248, 0.55) 0px 5px 50px;
       @media (max-width: 480px) {
         width: 18rem;
+        height: 2rem;
         font-size: 0.85rem;
         padding: 0.4rem;
         letter-spacing: 0.03em;
